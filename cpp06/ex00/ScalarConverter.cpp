19bbfe36b7e50b1bb473a42f076dfbebe485c8dc
sknefi <filip.karika1@gmail.com>
2025-12-21 21:31:29 +0100

    06 00 fixed typo

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -25,9 +25,9 @@ void	ScalarConverter::convert( std::string const &literal )
 		std::cout << std::fixed << std::setprecision(1); // float and double will have 1 decimal in print
 
 		if (std::isprint(literal[0]))
-			std::cout	<< "char: "		<< literal[0]						<< std::endl; 
+			std::cout	<< "char: '"	<< literal[0] << "'" 				<< std::endl; 
 		else
-			std::cout	<< "char: not_displayable"							<< std::endl;
+			std::cout	<< "char: Non displayable"							<< std::endl;
 		std::cout		<< "int: "		<< static_cast<int>(literal[0])		<< std::endl;
 		std::cout		<< "float: "	<< static_cast<float>(literal[0])	<< "f" << std::endl;
 		std::cout		<< "double: "	<< static_cast<double>(literal[0])	<< std::endl;
@@ -57,9 +57,9 @@ void	ScalarConverter::convert( std::string const &literal )
 	if (std::isnan(d) || std::isinf(d) || d < 0 || d > 127)
 		std::cout << "char: impossible"						<< std::endl;
 	else if (!std::isprint(static_cast<char>(d)))
-		std::cout << "char: not_displayable"				<< std::endl;
+		std::cout << "char: Non displayable"				<< std::endl;
 	else
-		std::cout << "char: " << static_cast<char>(d)		<< std::endl;
+		std::cout << "char: '" << static_cast<char>(d)		<< "'" << std::endl;
 	
 	// int
 	if (std::isnan(d) || std::isinf(d) || d < std::numeric_limits<int>::min() || d > std::numeric_limits<int>::max())
