bf32c03a708030601cf00d4f22b185ac02d106c4
sknefi <filip.karika1@gmail.com>
2025-12-23 20:35:35 +0100

    07 00 done

---
+++ cpp07/ex00/main.cpp
@@ -0,0 +1,35 @@
+#include "whatever.hpp"
+#include <iostream>
+
+int		main()
+{
+	int a = 2;
+	int b = 3;
+	
+	std::cout << "====== test int ======" << std::endl;
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	::swap(a, b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+
+	std::cout << "\n====== test float ======" << std::endl;
+	float c = 2.5f;
+	float d = 42.5f;
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+	
+	std::cout << "\n====== test string ======" << std::endl;
+	std::string e = "hello1";
+	std::string f = "hello2";
+	std::cout << "e = " << e << ", f = " << f << std::endl;
+	::swap(e, f);
+	std::cout << "e = " << e << ", f = " << f << std::endl;
+	std::cout << "min( e, f ) = " << ::min(e, f) << std::endl;
+	std::cout << "max( e, f ) = " << ::max(e, f) << std::endl;
+	
+	return (0);
+}
