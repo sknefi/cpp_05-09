0c87e88c10c6a9c3cf19f3c4a3056326c7437bdc
sknefi <filip.karika1@gmail.com>
2025-12-23 20:38:38 +0100

    07 00 added 42 test

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -3,33 +3,28 @@
 
 int		main()
 {
+	std::cout << "====== 42 TEST ======" << std::endl;
 	int a = 2;
 	int b = 3;
-	
-	std::cout << "====== test int ======" << std::endl;
+	::swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
-	::swap(a, b);
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
-	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
-
-	std::cout << "\n====== test float ======" << std::endl;
-	float c = 2.5f;
-	float d = 42.5f;
-	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
 	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
-	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 	
-	std::cout << "\n====== test string ======" << std::endl;
-	std::string e = "hello1";
-	std::string f = "hello2";
-	std::cout << "e = " << e << ", f = " << f << std::endl;
-	::swap(e, f);
-	std::cout << "e = " << e << ", f = " << f << std::endl;
-	std::cout << "min( e, f ) = " << ::min(e, f) << std::endl;
-	std::cout << "max( e, f ) = " << ::max(e, f) << std::endl;
+	std::cout << "\n====== MY TEST ======" << std::endl;
+	float g = 42.42f;
+	float h = 21.21f;
+	std::cout << "g = " << g << ", h = " << h << std::endl;
+	::swap(g, h);
+	std::cout << "g = " << g << ", h = " << h << std::endl;
+	std::cout << "min( g, h ) = " << ::min(g, h) << std::endl;
+	std::cout << "max( g, h ) = " << ::max(g, h) << std::endl;
 	
 	return (0);
 }
