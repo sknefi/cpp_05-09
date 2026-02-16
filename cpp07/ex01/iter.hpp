583af8f8ef4cd2f6f22a70434fa7ab96bf84c762
sknefi <filip.karika1@gmail.com>
2026-02-16 14:25:57 +0100

    07 01 fixed const

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -1,9 +1,19 @@
 #pragma once
 
 #include <iostream>
+#include <cstddef>
 
 template <typename T>
-void	iter( T *array, size_t length, void (*f)(T const &) )
+void	iter( T *array, size_t length, void (*f)(T &) )
+{
+	if (!array || !f)
+		return ;
+	for (size_t i = 0; i < length; i++)
+		f(array[i]);
+}
+
+template <typename T>
+void	iter( T const *array, size_t length, void (*f)(T const &) )
 {
 	if (!array || !f)
 		return ;
