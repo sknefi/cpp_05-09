fda12656e7a91061861509f0ec53c477a149cd96
sknefi <filip.karika1@gmail.com>
2026-02-17 15:54:37 +0100

    09 02 random numbers generator py

--- cpp09/ex02/generate_nums.py
+++ cpp09/ex02/generate_nums.py
@@ -3,4 +3,8 @@ x = 7919
 for i in range(3000):
     x = (x * 1103515245 + 12345) & 0x7fffffff
     nums.append(1 + (x % 100000))
-print("./PmergeMe " + " ".join(map(str, nums)))
+
+numbers_line = " ".join(map(str, nums))
+
+with open("numbers.txt", "w") as f:
+    f.write(numbers_line + "\n")
